4eae358c4ac8f91f229576fb8062d1e8e88be38d
mva7 <mva7@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-04 22:41:31 +0000

    upgrading to 1.9
    git-svn-id: https://plugins.svn.wordpress.org/the-holiday-calendar/trunk@1127972 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- constants/constants.class.php
+++ constants/constants.class.php
@@ -1,7 +1,7 @@
 <?php
 class thc_constants
 {
-	const PLUGIN_VERSION = '1.8.1';
+	const PLUGIN_VERSION = '1.9';
 	const POSTTYPE = 'thc-events';
 	const EVENTS_SLUG = 'events';
 }
--- the-holiday-calendar.php
+++ the-holiday-calendar.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: The Holiday Calendar
-Version: 1.8.1
+Version: 1.9
 Plugin URI: http://www.theholidaycalendar.com
 Description: Shows the upcoming holidays.
 Author: Mva7
